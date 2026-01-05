dd03caf1368c27fe89ff62fa15dcc16cbf4f10e4
nantaphop-ehe <156408676+nantaphop-ehe@users.noreply.github.com>
2026-01-05 08:13:27 +0000

    test 1

--- egco344/app.js
+++ egco344/app.js
@@ -1,14 +1,15 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('ehe te nandayo\n');          
-// });
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('ehe te nandayo\n');          
+});
 
-// const port = process.env.PORT || 8080; 
-// server.listen(port,() => {
-//   console.log(`Listening on port ${port}`);
-// });
+const port = process.env.PORT || 8080; 
+
+server.listen(port,() => {
+  console.log(`Listening on port ${port}`);
+});
 
 // Mock student data
 const students = [
